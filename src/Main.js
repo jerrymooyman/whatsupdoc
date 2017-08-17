c764b670f902745e65fe856dd4cac903bbb79143
Jerry Mooyman <jamooyman@gmail.com>
2017-08-17 12:30:23 +1000

    implemented auth0 lock to allow signup/login

---
+++ src/Main.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import {
+  Alert,
+  AppRegistry,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Auth0 from 'react-native-auth0';
+
+var credentials = require('./auth0-credentials');
+const auth0 = new Auth0(credentials);
+
+export default class whatsupdoc extends Component {
+  _onLogin() {
+    auth0
+        .webAuth
+        .authorize({scope: 'openid email', audience: 'https://' + credentials.domain + '/userinfo'})
+        .then(credentials =>
+              Alert.alert(
+                  'Success',
+                  'AccessToken: ' + credentials.accessToken,
+                  [
+                    {text: 'OK', onPress: () => console.log('OK Pressed')},
+                  ],
+                  { cancelable: false }
+                ))
+        .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Auth0Sample - Login
+        </Text>
+        <Button onPress={this._onLogin} title="Log In" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+AppRegistry.registerComponent('whatsupdoc', () => whatsupdoc);
---
+++ src/auth0-credentials.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientId: "lMoT1ahTskeOxVfYiQez2qhIJw8FQpYA",
+  domain: "derp.au.auth0.com"
+}
